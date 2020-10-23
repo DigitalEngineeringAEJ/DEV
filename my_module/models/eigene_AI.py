b487128db53ab1afd4147f813073a22267336234
DigitalEngineeringAEJ <management@aejgmbh.com>
2020-10-23 17:51:52 +0000

    Algorithmus

---
+++ my_module/models/eigene_AI.py
@@ -0,0 +1,22 @@
+#Schauen wir mal 
+
+from odoo import models, fields, api
+
+
+class my_module(models.Model):
+     _name = 'my_module.my_AI'
+     _description = 'my_module.my_AI'
+     Anzahl_Infizierte = fields.Integer()
+     Faktor = fields.Float()
+     Population = fields.Integer()
+     DX = fields.Float(compute="_calc_AI", store=True)
+    
+    
+     @api.depends('Ergebnis')
+     def _calc_AI(self):
+         for record in self:
+             record.DX = integer(record.Anzahl_Infizierte) * float(record.Faktor) / integer(record.Population)
+    
+    
+    
+    
