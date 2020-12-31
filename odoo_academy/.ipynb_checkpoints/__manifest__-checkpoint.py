34b9811216817282d113353b6d94c907c2cace85
DigitalEngineeringAEJ <management@aejgmbh.com>
2020-12-31 10:46:23 +0000

    Test

---
+++ odoo_academy/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Odoo Academy',
+    
+    'summary':"Erster Test für die Schulung",
+    
+    'description': """
+    Academy Modul to test:
+    - Course
+    - Session
+    """,
+    
+    'author': 'AEJ',
+    
+    'website': 'https://www.odoo.com',
+    
+    'category': 'Traning',
+    'version' : '0.1',
+    
+    'depends': ['base'],
+    'data': [
+        
+    ],
+    
+    'demo':[
+       'demo/academy_demo.xml' 
+    ],
+
+}
